08cd48b2985026c2590eee4f4299760b05a6ce59
risto <risto.kowaczewski@gmail.com>
2021-08-16 16:43:01 +0200

    async client

--- tracardi_mongodb_connector/model/client.py
+++ tracardi_mongodb_connector/model/client.py
@@ -9,10 +9,6 @@ class MongoClient:
         self.client = AsyncIOMotorClient(config.uri, serverSelectionTimeoutMS=config.timeout)
 
     async def find(self, database, collection, query):
-        async def _fetch(cursor):
-            async for document in cursor:
-                yield document
-
         database = self.client[database]
         collection = database[collection]
         return [data async for data in collection.find(query)]
